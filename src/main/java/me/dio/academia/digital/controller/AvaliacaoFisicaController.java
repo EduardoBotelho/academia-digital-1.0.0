766ed0ccc03e5824281e58a94da0b5c61938b42b
Eduardo Botelho <edu.botelho@yahoo.com>
2023-03-30 11:05:39 -0300

    Salvando no banco de dados a AvaliacaoFisica.

--- src/main/java/me/dio/academia/digital/controller/AvaliacaoFisicaController.java
+++ src/main/java/me/dio/academia/digital/controller/AvaliacaoFisicaController.java
@@ -1,9 +1,22 @@
 package me.dio.academia.digital.controller;
 
+import me.dio.academia.digital.Impl.AvaliacaoFisicaControllerImpl;
+import me.dio.academia.digital.entity.AvaliacaoFisica;
+import me.dio.academia.digital.entity.form.AvaliacaoFisicaForm;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/avaliacoes")
 public class AvaliacaoFisicaController {
+
+    private AvaliacaoFisicaControllerImpl service;
+
+    @PostMapping
+    public AvaliacaoFisica create(@RequestBody AvaliacaoFisicaForm form){
+        return service.create(form);
+    }
+
 }
